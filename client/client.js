6573ba83522e685085c559796a58f83ca45b2c0c
MiguelDotL <miguel.d.lozano@gmail.com>
2016-09-02 16:18:23 -0400

    Create bundle.js, App.js, & Setup webpack.config.js

--- client/client.js
+++ client/client.js
@@ -1,1 +1,10 @@
+import React from 'react'     //  var React = require('react');
+import { render } from 'react-dom'  //  var render = require('react-dom').render;
+import App from '../components/App'
 
+
+render(
+  <App/>,                           // define the encompasing component,
+
+  document.getElementById("app")    // DOM element to mount to
+)
